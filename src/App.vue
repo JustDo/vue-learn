1e988731fdef23d969e742dbe83c73f0c195e127
BruceGavin <bruce_gavin@163.com>
2020-05-26 09:07:40 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!-- 暂时没用 -->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
